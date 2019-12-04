4e68ca730efa0044ad4e67c4d94d3ef94984b54f
Asher Kaufman <bph436@zebra.com>
2019-12-04 15:42:49 -0500

    cleaned up d4_p2

--- d4_p2/src/main.rs
+++ d4_p2/src/main.rs
@@ -8,9 +8,16 @@ fn main() {
     let dup = Regex::new("(^|[^1])1{2}([^1]|$)|(^|[^2])2{2}([^2]|$)|(^|[^3])3{2}([^3]|$)|(^|[^4])4{2}([^4]|$)|(^|[^5])5{2}([^5]|$)|(^|[^6])6{2}([^6]|$)|(^|[^7])7{2}([^7]|$)|(^|[^8])8{2}([^8]|$)|(^|[^9])9{2}([^9]|$)").unwrap();
     let asc = Regex::new("^1*2*3*4*5*6*7*8*9*$").unwrap();
 
-    let passwords: Vec<i32> = (min..max)
-    .filter(|x| dup.is_match(&x.to_string()) && asc.is_match(&x.to_string()))
-    .map(|x| x)
-    .collect();
-    println!("{}",passwords.len());
+    // let passwords: Vec<i32> = (min..max)
+    // .filter(|x| dup.is_match(&x.to_string()) && asc.is_match(&x.to_string()))
+    // .map(|x| x)
+    // .collect();
+
+    // let passwords: Vec<i32> = (min..max)
+    // .filter(|x| dup.is_match(&x.to_string()) && asc.is_match(&x.to_string()))
+    // .collect();
+
+    let passwords = (min..max)
+    .filter(|x| dup.is_match(&x.to_string()) && asc.is_match(&x.to_string())).count();
+    println!("{}",passwords);
 }
